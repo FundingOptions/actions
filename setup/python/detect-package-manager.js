1414c71a822cace6e723e733756a490808b29b72
Anthony King <anthonydking@gmail.com>
2021-11-16 18:04:02 +0000

    feat(setup-python): add an action for configuring python environments
    - sets up python
    - sets up pip caching
    - detects package manager (poetry/pipenv supported only)
    - sets up package manager
    - sets up package manager caching

---
+++ setup/python/detect-package-manager.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+
+
+function isPipenv(directory) {
+  return (
+    fs.existsSync(path.join(directory, 'Pipenv'))
+    && fs.existsSync(path.join(directory, 'Pipfile.lock'))
+  );
+}
+
+function isPoetry(directory) {
+  return (
+    fs.existsSync(path.join(directory, 'pyproject.toml'))
+    && fs.existsSync(path.join(directory, 'poetry.lock'))
+  )
+}
+
+function detectManager(directory) {
+  if (isPipenv(directory)) {
+    return "pipenv";
+  } else if (isPoetry(directory)) {
+    return "poetry";
+  } else {
+    return "unknown";
+  }
+}
+
+function getDependencies(manager) {
+  switch (manager) {
+    case "pipenv":
+      return "pipenv";
+    case "poetry":
+    
+  }
+}
+
+module.exports = async ({ core }) => {
+  workspace = process.env.WORKSPACE;
+  if (!workspace) {
+    throw Error("`env.WORKSPACE` not set");
+  }
+
+  const manager = detectManager(workspace);
+  core.setOutput("using", manager);
+}
